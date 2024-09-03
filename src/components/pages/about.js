57b03a7113dc7ef2311827eb77ff12d658020dfa
charliharrold <charliharrold@gmail.com>
2024-09-02 21:47:12 -0700

    Added information to the About page.

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -3,18 +3,72 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Footer from '../footer/footer';
 
+import tote2 from '../../resources/Tote Bag 2.jpg';
+import womenEngCelebration from '../../resources/Women in Eng Celebration.jpg';
+
+const hrStyle = {
+    border: 'none',
+    height: '5px',
+    backgroundColor: '#ff9df5',
+  };
+
 function About() {
-  return (
-    <div>
-    <h1>My About Page</h1>
-    <Container fluid>
-      <Row>
-        <Col>1 of 1</Col>
-      </Row>
-    </Container>
-    <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <h1 style={{ textAlign: "center" }}>About Us!</h1>
+            <div className={"container"}>
+                <div className={"row"}>
+                    <div className={"col"}>
+                        <p style={{ fontSize: "20px" }}> Women in Engineering and Computer Science (WECS) at the University of Victoria strives to improve the experience of women and gender-non-conforming individuals studying engineering and computer science.
+                            <br></br>
+                            <br></br>
+                            Our group has evolved many times over the last decade, always with a goal to create a community for our members. Past executives have hosted Tea Times, Study Hours, and Review Sessions. We will continue to grow our team to provide opportunities for WECS members.
+                        </p>
+                    </div>
+                </div>
+                <hr style={hrStyle}/>
+
+                <div className={"row"}>
+                    <div className={"col"}>
+                        <h2>Our Goal</h2>
+                        <p style={{ fontSize: "20px" }}>
+                            Women in Engineering and Computer Science (WECS) has a goal to create a community for women and gender-non-conforming individuals at the University of Victoria to grow as students and professionals.</p>
+                    </div>
+                    <div className={"col-6"} >
+                        <img src={tote2} width='500' height='667' class="img-fluid" />
+                    </div>
+                </div>
+
+                <hr style={hrStyle} />
+
+                <div className={"row"}>
+
+                    <div className={"col-6"} >
+                        <img src={womenEngCelebration} width='500' height='667' class="img-fluid" />
+                    </div>
+                    <div className={"col"}>
+                        <h2>Our Focus</h2>
+                        <p style={{ fontSize: "20px" }}>
+                            We have three core areas of focus: Academia, Professional Development, and Community. Within each focus, we provide low-commitment opportunities to support our members.
+                            <br></br>
+                            <br></br>
+                            Example of opportunities we host include (but are not limited to):
+                            <br></br>
+                            Academia: review sessions and study hours
+                            <br></br>
+                            Professional Development: networking events and speaker series
+                            <br></br>
+                            Community: tote bag paint night and exam fuel and chill
+                        </p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <Footer />
+        </div>
+    );
 }
 
 export default About;
