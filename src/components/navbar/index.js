9be8e0db1be71b95fff1c83bb503ba6c82fc5ece
John Lennox <jl1998@hotmail.co.uk>
2024-10-02 18:22:37 +0100

    adding use effect and uselocation to the nav bar
    adding listener to the URL that listens for a change and sets the active key in the nav bar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import wecs from '../../resources/WECS Logo.png'
-import logoWhiteBackground from '../../resources/Logo White Background.png';
+import {useLocation} from 'react-router-dom';
+import wecs from '../../resources/WECS Logo.png';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
@@ -16,9 +16,24 @@ const makePink = {
 function Navigation() {
     const [activeKey, setActiveKey] = useState('#home');
 
+    // this allows us to get the URL from the browser.
+    const location = useLocation();
+
     const handleBrandClick = () => {
         setActiveKey('#home');
     };
+
+    // This useEffect method will be triggered every time the location variable changes
+    useEffect(() => {
+        if (location.pathname === '/') {
+            // There is nothing after the normal url, so we should go to the home page
+            setActiveKey('#home');
+        } else {
+            // when the URL is at the about page, location.pathname will be /about,
+            // because of the # routing we want to replace the / with a #
+            setActiveKey(location.pathname.replace('/', '#'));
+        }
+    }, [location]);
 
 
     return (
