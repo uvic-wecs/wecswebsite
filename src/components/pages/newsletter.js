f0032a442ff08987aa8eb93b736dd678608ac4e4
epalter <epalter@uvic.ca>
2025-03-19 11:18:32 -0700

    spacing

--- src/components/pages/newsletter.js
+++ src/components/pages/newsletter.js
@@ -48,7 +48,8 @@ const Newsletter = () => {
                         Whether you're falling for finite element analysis, crushing on control systems, or just trying to stay on good terms with your compiler, we’re here to add some sweetness to your month. 
                         From exciting WECS events (like our upcoming Gal-entines event) to open positions and community support, consider this your friendly reminder that you’re not debugging or stress-testing life alone. 
                         Wishing you a semester full of good vibes, great ideas, and low structural strain!
-                            Continue reading <a href="https://us21.campaign-archive.com/?u=f1586a8f88c058352d03ee000&id=981abb26c7">here</a>!
+                        <br /><br />
+                        Continue reading <a href="https://us21.campaign-archive.com/?u=f1586a8f88c058352d03ee000&id=981abb26c7">here</a>!
                         </p>
                     </Col>
                     <Col xs={12} md={4}>
@@ -68,8 +69,8 @@ const Newsletter = () => {
                         As we hit the ground running with this new term,  whether your New Year’s resolution is to spend more time in the library, 
                         or to never step foot in the library again, I'm here to tell you about the fun WECS events coming up, open positions with us and 
                         remind you that we’ve got your back and are wishing you your best semester yet!
-                            <br /><br />
-                            Continue reading <a href="https://us21.campaign-archive.com/?u=f1586a8f88c058352d03ee000&id=51e62cb39c">here</a>!
+                        <br /><br />
+                        Continue reading <a href="https://us21.campaign-archive.com/?u=f1586a8f88c058352d03ee000&id=51e62cb39c">here</a>!
                         </p>
                     </Col>
                 </Row>
@@ -79,13 +80,13 @@ const Newsletter = () => {
             <h4>Older Issues</h4>
             <p>
                 Check out the December 2024 edition <a href="https://us21.campaign-archive.com/?u=f1586a8f88c058352d03ee000&id=f10ed27d8b">here</a>!
-                <br /><br />
+                <br />
                 Check out the November 2024 edition <a href="https://us21.campaign-archive.com/?u=f1586a8f88c058352d03ee000&id=fdaa601c48">here</a>!
-                <br /><br />
+                <br />
                 Check out the October 2024 edition <a href="https://us21.campaign-archive.com/?u=f1586a8f88c058352d03ee000&id=b5919fa0f6">here</a>!
-                <br /><br />
+                <br />
                 Check out the Bonus September 2024 edition <a href="https://us21.campaign-archive.com/?u=f1586a8f88c058352d03ee000&id=86e9a7db0e">here</a>!
-                <br /><br />
+                <br />
                 Check out the September 2024 edition <a href="https://us21.campaign-archive.com/?u=f1586a8f88c058352d03ee000&id=1cd27c8dae">here</a>!
             </p>
         </div>
