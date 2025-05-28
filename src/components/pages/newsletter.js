25e3e8a944c628575237319c3d66449f9822e938
epalter <epalter@uvic.ca>
2025-05-27 17:11:10 -0700

    made some tweaks based on charli's comments but need to fix more things and make all images same size

--- src/components/pages/newsletter.js
+++ src/components/pages/newsletter.js
@@ -2,36 +2,59 @@ import React from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import march25 from "../../resources/newsletters/march25.png";
 import feb25 from "../../resources/newsletters/feb25.png";
+import jan25 from "../../resources/newsletters/jan25.png";
 import apr25 from "../../resources/newsletters/apr25.png";
+import { Link } from "react-router-dom";
 
 const issues = [
   {
+    title: 'April Issue',
+    semester: 'Spring 2025',
+    image: apr25,
+    description: `And just like that, another semester comes to an end! As we wrap things up, WECS is once again honoured to offer a wide variety of Final Exam Review Sessions to help you feel as confident as possible going into your exams. Keep reading for the full list of upcoming sessions—plus a few extra surprises along the way. From all of us, we wish you a fantastic exam semester. We’re here to help—and to remind you to take some moments to relax, recharge in the sun, celebrate Earth Day, and keep an eye out for George the Peacock on his daily walk!`,
+    link: 'https://us21.campaign-archive.com/?u=f1586a8f88c058352d03ee000&id=f28300cfee'
+  },
+  {
     title: 'March Issue',
-    season: 'Spring 2025',
+    semester: 'Spring 2025',
     image: march25,
     description: `Spring has sprung! No more trudging to your 8:30 AM in the dark or stepping out of ECS at 5 PM only to find it's basically midnight. As campus bursts into colour—from daffodils to cherry blossoms (and that one dramatic purple tree by the fountain)—WECS is here to help you bloom too! We’ve got exciting opportunities, incredible events, and more coming your way! So grab your sunglasses, find a sunny spot on the quad, and dive in.`,
     link: 'https://us21.campaign-archive.com/?u=f1586a8f88c058352d03ee000&id=5f9cd1fb98'
   },
   {
     title: 'February Issue',
-    season: 'Spring 2025',
+    semester: 'Spring 2025',
     image: feb25,
     description: `Love is in the air - and so are midterms! Welcome to the February edition of The WECS Scoop! Whether you're falling for finite element analysis, crushing on control systems, or just trying to stay on good terms with your compiler, we’re here to add some sweetness to your month. From exciting WECS events (like our upcoming Gal-entines event) to open positions and community support, consider this your friendly reminder that you’re not debugging or stress-testing life alone. Wishing you a semester full of good vibes, great ideas, and low structural strain!`,
     link: 'https://us21.campaign-archive.com/?u=f1586a8f88c058352d03ee000&id=981abb26c7'
   },
   {
     title: 'January Issue',
-    season: 'Spring 2025',
+    semester: 'Spring 2025',
     image: jan25,
     description: `Welcome to the first WECS Scoop of 2025! I hope your break was restful, fun and everything you needed! As we hit the ground running with this new term, whether your New Year’s resolution is to spend more time in the library, or to never step foot in the library again, I'm here to tell you about the fun WECS events coming up, open positions with us and remind you that we’ve got your back and are wishing you your best semester yet!`,
     link: 'https://us21.campaign-archive.com/?u=f1586a8f88c058352d03ee000&id=51e62cb39c'
   }
 ];
 
+const removeLinkStyling = {
+  textDecoration: 'none',
+  color: '#000000',
+}
+
+// will scroll back to the top of the page when you click on a link
+const onClickScroll = () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
 export default function Newsletter() {
   return (
-    <Container className="py-5">
-      <header className="text-center mb-5">
+    <Container className="py-2">
+      <header className="text-center mb-3">
         <h1>WECS Newsletter</h1>
         <p className="lead">
           Keep up with resources, opportunities, and events through WECS each month.
@@ -43,21 +66,20 @@ export default function Newsletter() {
       </header>
 
       <h2 className="mb-4">Spring 2025 - Check out what we've been up to this semester!</h2>
-      <Row xs={1} md={3} className="g-4">
+      <Row xs={1} md={2} className="g-4">
         {issues.map((issue, idx) => (
           <Col key={idx}>
             <Card className="h-100 shadow-sm">
-              <Card.Img variant="top" src={issue.image} alt={issue.title} className="img-fluid" />
-              <Card.Body>
-                <Card.Title>{issue.title}</Card.Title>
-                <Card.Text>{issue.description}</Card.Text>
-                <Button variant="primary" href={issue.link} target="_blank">
-                  Read More
-                </Button>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Season: {issue.season}</small>
-              </Card.Footer>
+              <Link to={issue.link} onClick={onClickScroll} style={removeLinkStyling}>
+                <Card.Img variant="top" src={issue.image} alt={issue.title} className="img-fluid" />
+                <Card.Body>
+                  <Card.Title>{issue.title}</Card.Title>
+                  <Card.Text>{issue.description}</Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">semester: {issue.semester}</small>
+                </Card.Footer>
+              </Link>
             </Card>
           </Col>
         ))}
