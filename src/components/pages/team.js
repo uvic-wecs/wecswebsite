2edc2c776920cb8dfad001603607010eee3c465b
cmcneice <cmcneice@uvic.ca>
2025-09-14 02:15:04 -0700

    changed photo format to webp.

--- src/components/pages/team.js
+++ src/components/pages/team.js
@@ -12,8 +12,8 @@ import Ella from "../../resources/executives/Ella.webp";
 import Malsha from "../../resources/executives/Malsha.webp";
 import Sydney from "../../resources/executives/Sydney.webp";
 import Alice from "../../resources/executives/Alice.webp";
-import Christina from "../../resources/executives/Christina.avif";
-import Natasha from "../../resources/executives/Natasha.avif";
+import Christina from "../../resources/executives/Christina.webp";
+import Natasha from "../../resources/executives/Natasha.webp";
 import Amy from "../../resources/executives/Amy.webp";
 import Helena from "../../resources/executives/Helena.webp";
 import Fareess from "../../resources/executives/Fareess.webp";
@@ -22,6 +22,13 @@ import Pavit from "../../resources/executives/Pavit.webp";
 import Emma from "../../resources/executives/Emma.webp";
 import Tanvi from "../../resources/executives/Tanvi.webp";
 import Natalie from "../../resources/executives/Natalie.webp";
+
+import Cathy from "../../resources/executives/Cathy.webp";
+import Dilpreet from "../../resources/executives/Dilpreet.webp";
+import Romi from "../../resources/executives/Romi.webp";
+import Aashna from "../../resources/executives/Aashna.webp";
+import Rodiat from "../../resources/executives/Rodiat.webp";
+
 
 import wecsLogo from "../../resources/executives/WECS Logo.webp";
 import pinkBackground from "../../resources/executives/Pink Powder.webp";
@@ -86,25 +93,26 @@ const marloBio = {
   src: pinkBackground, title: <p>Professional Development Events Director - Marlo</p>, description: <p></p>, shortDescription: <p></p>
 };
 const romiBio = {
-  src: pinkBackground, title: <p>Website Director - Romi</p>, description: <p><br /><br /> <br /><br /></p>, shortDescription: <p><br /><br /> <br /><br /></p>
+  src: Romi, title: <p>Website Director - Romi</p>, description: <p>I'm someone who loves exploring both logic and creativity — whether it's through writing code or cooking up something delicious in the kitchen. I enjoy spending my time solving problems with code, playing badminton to stay active, and discovering (or making) great food. I'm always up for a challenge, whether it's debugging a tricky program or trying out a new recipe!</p>, shortDescription: <p>I'm someone who loves exploring both logic and creativity — whether it's through writing code or cooking...</p>
 };
 const christinaMBio = {
-  src: pinkBackground, title: <p>Website Manager - Christina</p>, description: <p><br /><br /> <br /><br /></p>, shortDescription: <p><br /><br /><br /> <br /></p>
+  src: pinkBackground, title: <p>Website Manager - Christina</p>, description: <p>Hello! I'm Christina, a third-year Computer Science and Statistics student with a passion for developing community-driven technology and building meaningful, lasting relationships. When I'm not coding or analyzing data, you'll find me enjoying coffee with my cat Gizmo or exploring the beautiful coastline.</p>, shortDescription: <p>Hello! I'm Christina, a third-year Computer Science and Statistics student with a passion for developing community-driven technology...</p>
 };
 const rodiatBio = {
-  src: pinkBackground, title: <p>Editorial Director - Rodiat</p>, description: <p><br /><br /> <br /><br /></p>, shortDescription: <p><br /><br /> <br /><br /></p>
+  src: Rodiat, title: <p>Editorial Director - Rodiat</p>, description: <p>
+    Hi, I am Rodiat, I love being indoors, binging movies, reading comics, and sleeping, nice to meet you!</p>, shortDescription: <p>Hi, I am Rodiat, I love being indoors, binging movies, reading comics, and sleeping, nice to meet you!</p>
 };
 const layanBio = {
   src: pinkBackground, title: <p>Academic Materials Director - Layan</p>, description: <p><br /><br /><br /><br /></p>, shortDescription: <p><br /><br /><br /><br /></p>
 };
 const aashnaBio = {
-  src: pinkBackground, title: <p>VP Professional Development - Aashna</p>, description: <p></p>, shortDescription: <p></p>
+  src: Aashna, title: <p>VP Professional Development - Aashna</p>, description: <p>Hi! I’m Aashna, and I'm so excited to be your Engineering Residence Education Leader this year! I’m a second-year Computer Science student with a passion for traveling, reading, and politics. I'm super excited to be a part of the WECS team and help build a supportive community!</p>, shortDescription: <p>Hi! I’m Aashna, and I'm so excited to be your Engineering Residence Education Leader this year! I’m a second-year Computer Science student with a passion... </p>
 };
 const dilpreetBio = {
-  src: pinkBackground, title: <p>Funding Director - Dilpreet</p>, description: <p><br /><br /></p>, shortDescription: <p><br /><br /></p>
+  src: Dilpreet, title: <p> Corporate Outreach & Funding Director - Dilpreet</p>, description: <p>Hi everyone! I joined WECS in June 2025 and am a 2nd year undecided engineering student hoping to declare into mechanical engineering. I enjoy walking and hiking around Victoria. I hope to positively contribute to WECS’ success and growth! </p>, shortDescription: <p>Hi everyone! I joined WECS in June 2025 and am a 2nd year undecided engineering student hoping to... </p>
 };
 const cathyBio = {
-  src: pinkBackground, title: <p>Partnership Director - Cathy</p>, description: <p><br /><br /><br /></p>, shortDescription: <p><br /><br /><br /></p>
+  src: Cathy, title: <p>Partnership Director - Cathy</p>, description: <p>Hi! I'm Cathy, the partnership director for wecs! I joined in May 2025 and have run a wecs x ecss board game event so far. In my free time, I like to go swimming and draw. I'm excited to collaborate with more clubs to run cool events this year! </p>, shortDescription: <p>Hi! I'm Cathy, the partnership director for wecs! I joined in May 2025 and have run a wecs x ecss board game event...</p>
 };
 const muskanBio = {
   src: pinkBackground, title: <p>Workshop Director - Muskan</p>, description: <p></p>, shortDescription: <p></p>
