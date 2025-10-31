b1d0d717ccac14a5c0397efaef5f5fc10aba7d53
Romi Bohmra <romibohmra2@gmail.com>
2025-10-31 14:42:31 -0700

    google calendar integration

--- src/components/pages/events.js
+++ src/components/pages/events.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import MyCalendar from '../calendar/calendar';
+import React from "react";
+import MyCalendar from "../calendar/calendar";
+import './events.css';
+
 
 const Events = () => {
   return (
-    <div>
+    <div className="event-calendar">
       <h1>WECS Events Calendar</h1>
       <p>We're looking forward to seeing you at our upcoming events!</p>
-      <MyCalendar />
-      <br/>
+      <br />
+      <iframe
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FVancouver&showPrint=0&showTitle=0&mode=MONTH&src=dXZpY3dlY3Mub2ZmaWNpYWxAZ21haWwuY29t&src=MGMxZDE2YTQ1MjQ4NTRjMWZiYzcyNWI5ZDJmMmQ5ZjEyZmE1NjZhZTBkOGY1NjhjNWZlM2JlNWUzYTg4ODQ0MUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=M2ZlZTcyNmNjZDg4ZjVjNmI3MWZjZTcyZGJiNWRlOGI5NjQ1OGM0NTA0NTIzMzgzNzRiOWU5NWNmMDY5MzVmMkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uY2FuYWRpYW4jaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23d81b60&color=%237986cb&color=%238e24aa&color=%230b8043"
+        frameborder="0"
+      ></iframe>
+      <br />
     </div>
   );
 };
