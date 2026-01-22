8b0dac25408e3f0098474bac8f45dd030335eb11
Charli Harrold <charliharrold@gmail.com>
2026-01-22 13:00:51 -0800

    Added a couple spaces

--- src/components/pages/events.js
+++ src/components/pages/events.js
@@ -4,6 +4,7 @@ import "./events.css";
 const Events = () => {
   return (
     <div className="event-container">
+      <br/>
       <div className="event-calendar">
         <h1>WECS Events Calendar</h1>
         <p>We're looking forward to seeing you at our upcoming events!</p>
@@ -11,6 +12,8 @@ const Events = () => {
         <iframe title="WECS Events Calendar" src="https://calendar.google.com/calendar/embed?src=1632028d6ee05901b9bed86cd0ead38c9df6986dd3361299a56711051fb3ab3d%40group.calendar.google.com&ctz=America%2FVancouver" frameborder="0" scrolling="no"></iframe>
         <br />
       </div>
+      <br />
+      <br />
     </div>
   );
 };
