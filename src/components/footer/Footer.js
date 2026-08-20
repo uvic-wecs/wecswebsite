1e761b40a1f199d92d925c2b8d544d5c39533605
Charli Harrold <charliharrold@gmail.com>
2026-08-19 18:09:58 -0700

    Updating the footer.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <hr />
       <p>© {year} WECS. All rights reserved.</p>
       
-      <p>Created by Charli Harrold. Managed by Athaliah, Romi, Cammie, and Hana!</p>
+      <p>Managed by Athaliah, Cammie, and Hana!</p>
     </footer>
   );
 };
